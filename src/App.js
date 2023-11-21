199fc092c7f9ef3bf114eeb4ca847f37b1b027b7
Shahrzad <75183431+shahrzadblackfox@users.noreply.github.com>
2023-11-22 00:13:07 +0330

    basic design

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import {FaEye} from "react-icons/fa";
+
+function App() {
+  return (
+    <div className="wrapper">
+      <div className="input-fields">
+        
+          <span><FaEye /></span>
+          <input type="password"/>
+
+
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
